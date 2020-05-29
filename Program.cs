fe111850c5a0e1465c8642be5b3108da9d89c91d
hieupoo16022001 <54589137+hieupoo16022001@users.noreply.github.com>
2020-05-29 13:10:43 +0700

    lab4

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab4
+{
+   public class Program
+    {
+        static void Main(String[] args)
+        {
+            Circle c1 = new Circle(2.45, "Blue", 23);
+            Console.WriteLine("Circle before change" + c1.ToString());
+
+            c1.PColor = "Red";
+            c1.PWeight = 2.56;
+            Console.WriteLine("Circle after change: " + c1.ToString());
+            Console.ReadLine();
+        }
+    }
+}
